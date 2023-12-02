c6e53d01746d218e7b4af3b7ab3ec18dd4995526
moog <brentano.l@gmail.com>
2023-12-01 23:05:37 -0300

    day1 | Minor tweaks WIP

--- day1/day1.c
+++ day1/day1.c
@@ -10,7 +10,7 @@ int main(void) {
 	int first_value = 0;
 	int last_value = 0;
 	int current_value = 0;
-	char str_nums[][5] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+	char str_nums[][6] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
 	file_pointer = fopen("adventofcode.com_2023_day_1_input.txt", "r");
 	// file_pointer = fopen("test_values.txt", "r");
@@ -22,7 +22,7 @@ int main(void) {
 
 	while (fgets(value, 100, file_pointer )) {
 		// DEBUG MESSAGE
-		// printf("%s", value);
+		printf("%s", value);
 
 		first_value = 0;
 		current_value = 0;
@@ -36,12 +36,11 @@ int main(void) {
 				last_value = current_value;
 			}
 
-			// DEBUG MESSAGE
-			// printf("fv: %d  |  lv: %d\n\n", first_value, last_value);
 			ch = value[i];
 			if (isdigit(ch)){
 				// char digit - '0' -> Converts the digit value to integer
 				current_value = ch - '0';
+				printf("Current value: %d | FOUND in: %c\n", current_value, ch);
 			} else {
 				// Build a 5 string starting from i position on the string
 				char text_snippet[] = {
@@ -54,10 +53,11 @@ int main(void) {
 					'\0' };
 
 				// 9 Because of amount of numbers, 1-9
-				for (int i = 0; i < 9; i++){
+				for (int j = 0; j < 9; j++){
 					// != NULL means that the string is found inside the other.
-					if(strstr(text_snippet, str_nums[i]) != NULL){
-						current_value = i + 1;
+					if(strstr(text_snippet, str_nums[j]) != NULL){
+						printf("Added %d - %s | FOUND in: %s\n", j+1, str_nums[j], text_snippet);
+						current_value = j + 1;
 
 					}
 				}
@@ -65,6 +65,8 @@ int main(void) {
 
 			i++;
 		} while (ch != '\0');
+		// DEBUG MESSAGE
+		printf("fv: %d  |  lv: %d\n\n", first_value, last_value);
 		// Transform the first_value to decimal
 		sum += first_value * 10;
 		sum += last_value;
