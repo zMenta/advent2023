c4387c247f83971d939dd5e23463f1fdcc759985
moog <brentano.l@gmail.com>
2023-12-05 19:24:22 -0300

    day2 sucess

--- day2/day2.c
+++ day2/day2.c
@@ -22,16 +22,12 @@ int main(void){
 	}
 
 	while (fgets(text, 200, file_pointer)){
-		printf("--------------------------------------------------------------------\n");
 		char color_value_str[10];
 		char color_section_str[30];
 		char game_str[5];
 		int i = 0;
 		int j = 0;
-		int green_sum = 0;
-		int blue_sum = 0;
-		int red_sum = 0;
-		printf("%s", text);
+		int is_game_valid = 1;
 
 		// Get current game value
 		do {
@@ -42,13 +38,12 @@ int main(void){
 			i++;
 		}while (text[i] != ':');
 
+		// Get color values
 		j = 0;
 		int color_section_index = 0;
-		// Get color values
 		do {
 			if(isdigit(text[i])){
 				color_value_str[j] = text[i];
-				// printf("Cur color value: %s\n", color_value_str);
 				j++;
 			} else {
 				color_section_str[color_section_index] = text[i];
@@ -59,35 +54,33 @@ int main(void){
 				for(int z = 0; z < sizeof(str_colors); z++){
 					if(strstr(color_section_str, str_colors[z]) != NULL){
 						switch (z) {
-							case 0: green_sum += atoi(color_value_str);
+							case 0: if (atoi(color_value_str) > max_green) {
+										is_game_valid = 0;
+									}
 									break;
-							case 1: blue_sum += atoi(color_value_str);
+							case 1: if (atoi(color_value_str) > max_blue) {
+										is_game_valid = 0;
+									}
 									break;
-							case 2: red_sum += atoi(color_value_str);
+							case 2: if (atoi(color_value_str) > max_red) {
+										is_game_valid = 0;
+									}
 									break;
 						}
-						// printf("Color section: %s\n", color_section_str);
-						// printf("Color value: %s\n", color_value_str);
 					}
 				}
-
 				// Clean strings
 				memset(color_value_str, 0, sizeof(color_value_str));
 				memset(color_section_str, 0, sizeof(color_section_str));
 				j = 0;
 				color_section_index = 0;
 			}
-
 			i++;
 		}while (text[i] != '\0');
 
-		// Validate if the current game is valid
-		if (green_sum <= max_green && red_sum <= max_red && blue_sum <= max_blue) {
+		if (is_game_valid == 1) {
 			valid_games_sum += atoi(game_str);
-			printf("Valid!\n");
 		}
-		printf("Game number: %d | red: %d | green: %d | blue: %d\n\n", atoi(game_str), red_sum, green_sum, blue_sum);
 	}
-
 	printf("Valid games sum: %d\n", valid_games_sum);
 }
